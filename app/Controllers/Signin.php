9a78b734e07d43b8e002792f80e76580c1857a02
Fau <107357334+azizarjag09@users.noreply.github.com>
2023-07-15 15:11:33 +0700

    Update Signin.php
    menambahkan fungsi level biar bedakan user dan admin ketika login

--- app/Controllers/Signin.php
+++ app/Controllers/Signin.php
@@ -2,10 +2,21 @@
 
 namespace App\Controllers;
 use App\Models\User;
+use App\Models\UserModel;
 
 class Signin extends BaseController
 {
     protected $session;
+    protected $db;
+
+    public function __construct()
+    {
+        $this->session = session();
+        $this->db = \Config\Database::connect();
+        $this->session = \Config\Services::session();
+    }
+
+
     public function index()
     {
         $session = session();
@@ -30,23 +41,74 @@ class Signin extends BaseController
 
     public function login_auth()
     {
-        $email = $this->request->getPost('email');
-        $password = $this->request->getPost('password');
-        $userModel = new User();
-        $user = $userModel->getUserByEmail($email);
-        if ($user['email'] == $email && $user['password'] == $password) {
-            // Login berhasil, simpan informasi pengguna ke dalam session
-            $session = session();
-            $session->set('id', $user['id']);
-            return redirect()->to(base_url('/container'));
+        $users = new UserModel();
+        $email = $this->request->getVar('email');
+        $password = $this->request->getVar('password');
+        $dataUser = $users->where([
+            'email' => $email,
+        ])->first();
+
+        if ($dataUser) {
+            if (password_verify($password, $dataUser->password)) {
+                if ($dataUser->level == '1') {
+                    session()->set([
+                        'email' => $dataUser->email,
+                        'level' => $dataUser->level,
+                        'id' => $dataUser->id,
+                        'logged_in' => TRUE
+                    ]);
+                    return redirect()->to(base_url('admin/index'));
+                } elseif ($dataUser->level == '0') {
+                    session()->set([
+                        'email' => $dataUser->email,
+                        'level' => $dataUser->level,
+                        'id' => $dataUser->id,
+                        'logged_in' => TRUE
+                    ]);
+                    return redirect()->to(base_url('/container'));
+                } else {
+                    $session = session();
+                    // Invalid user level
+                    session()->setFlashdata('error', 'Invalid user level');
+                    return redirect()->to(base_url('home/index'));
+                }
+            } else {
+                $session = session();
+                $session->setFlashdata('salahpw', '<div class="alert alert-danger">
+                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                              <i class="icon-line-lock"></i>Maaf, Password Anda Salah<br>Silahkan coba kembali.</a>
+                            </div>');
+                return redirect()->to(base_url('home/salahpw'));
+            }
         } else {
-            $url = base_url('/form_login') . '?show_register=true';
-            echo '<script>
-                alert("Password Tidak Sama");
-                window.location.href = "' . $url . '";
-            </script>';
-        }
+            $session = session();
+            $session->setFlashdata('salah', '<div class="alert alert-danger">
+                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                              <i class="icon-line-lock"></i>Maaf, Username & Password Anda Salah<br>Silahkan coba kembali.</a>
+                            </div>');
+            return redirect()->to(base_url('home/salah'));
+        }        
     }
+
+    // public function login_auth()
+    // {
+    //     $email = $this->request->getPost('email');
+    //     $password = $this->request->getPost('password');
+    //     $userModel = new User();
+    //     $user = $userModel->getUserByEmail($email);
+    //     if ($user['email'] == $email && $user['password'] == $password) {
+    //         // Login berhasil, simpan informasi pengguna ke dalam session
+    //         $session = session();
+    //         $session->set('id', $user['id']);
+    //         return redirect()->to(base_url('/container'));
+    //     } else {
+    //         $url = base_url('/form_login') . '?show_register=true';
+    //         echo '<script>
+    //             alert("Password Tidak Sama");
+    //             window.location.href = "' . $url . '";
+    //         </script>';
+    //     }
+    // }
 
     public function logout()
     {
