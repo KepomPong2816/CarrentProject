ef57e57a54c512fafd58581ffda0116661da7036
Fau <107357334+azizarjag09@users.noreply.github.com>
2023-07-15 15:00:17 +0700

    Update pesan_tiket.php

--- app/Views/pesan_tiket.php
+++ app/Views/pesan_tiket.php
@@ -22,24 +22,96 @@
                         <div class="row">
                             <div class="col-11">
                                 <div class="d-block">
-                                    <label for="inputState" class="form-label">Lokasi Rental</label>
+                                    <label for="inputState" class="form-label">Lokasi Asal</label>
                                     <select id="inputState" class="form-select">
                                         <option selected></option>
-                                        <option>Jakarta</option>
-                                        <option>Bandung</option>
-                                        <option>Yogyakarta</option>
-                                        <option>Surabaya</option>
-                                        <option>Semarang</option>
-                                        <option>Malang</option>
-                                        <option>Bogor</option>
-                                        <option>Solo</option>
+                                        <option value="Jakarta">Jakarta</option>
+                                        <option value="Bandung">Bandung</option>
+                                        <option value="Semarang">Semarang</option>
+                                        <option value="Yogyakarta">Yogyakarta</option>
+                                        <option value="Surabaya">Surabaya</option>
+                                        <option value="Surakarta">Surakarta</option>
+                                        <option value="Malang">Malang</option>
+                                        <option value="Cirebon">Cirebon</option>
+                                        <option value="Cimahi">Cimahi</option>
+                                        <option value="Depok">Depok</option>
+                                        <option value="Bekasi">Bekasi</option>
+                                        <option value="Tangerang">Tangerang</option>
+                                        <option value="Bogor">Bogor</option>
+                                        <option value="Magelang">Magelang</option>
+                                        <option value="Salatiga">Salatiga</option>
+                                        <option value="Solo">Solo</option>
+                                        <option value="Probolinggo">Probolinggo</option>
+                                        <option value="Pasuruan">Pasuruan</option>
+                                        <option value="Madiun">Madiun</option>
+                                        <option value="Kediri">Kediri</option>
+                                        <option value="Jember">Jember</option>
+                                        <option value="Banyuwangi">Banyuwangi</option>
+                                        <option value="Purwokerto">Purwokerto</option>
+                                        <option value="Cilacap">Cilacap</option>
+                                        <option value="Pekalongan">Pekalongan</option>
+                                        <option value="Tegal">Tegal</option>
+                                        <option value="Brebes">Brebes</option>
+                                        <option value="Karawang">Karawang</option>
+                                        <option value="Indramayu">Indramayu</option>
+                                        <option value="Cirebon">Cirebon</option>
+                                        <option value="Purwakarta">Purwakarta</option>
+                                        <option value="Sukabumi">Sukabumi</option>
+                                        <option value="Cianjur">Cianjur</option>
+                                        <option value="Bogor">Bogor</option>
+                                        <option value="Banjar">Banjar</option>
+                                        <option value="Ciamis">Ciamis</option>
+                                        <option value="Cirebon">Cirebon</option>
+                                        <option value="Tasikmalaya">Tasikmalaya</option>
+                                        <option value="Garut">Garut</option>
+                                        <option value="Pangandaran">Pangandaran</option>
                                     </select>
                                 </div>
-                                <div class="d-block ">
-                                    <div class="mb-3">
-                                        <label for="formGroupExampleInput" class="form-label">Kota Tujuan</label>
-                                        <input type="text " class="form-control " id="formGroupExampleInput" placeholder="Lokasi Tempat Tujuan Anda">
-                                    </div>
+                                <div class="d-block">
+                                    <label for="inputState" class="form-label">Tujuan</label>
+                                    <select id="inputState" class="form-select">
+                                        <option selected></option>
+                                        <option value="Jakarta">Jakarta</option>
+                                        <option value="Bandung">Bandung</option>
+                                        <option value="Semarang">Semarang</option>
+                                        <option value="Yogyakarta">Yogyakarta</option>
+                                        <option value="Surabaya">Surabaya</option>
+                                        <option value="Surakarta">Surakarta</option>
+                                        <option value="Malang">Malang</option>
+                                        <option value="Cirebon">Cirebon</option>
+                                        <option value="Cimahi">Cimahi</option>
+                                        <option value="Depok">Depok</option>
+                                        <option value="Bekasi">Bekasi</option>
+                                        <option value="Tangerang">Tangerang</option>
+                                        <option value="Bogor">Bogor</option>
+                                        <option value="Magelang">Magelang</option>
+                                        <option value="Salatiga">Salatiga</option>
+                                        <option value="Solo">Solo</option>
+                                        <option value="Probolinggo">Probolinggo</option>
+                                        <option value="Pasuruan">Pasuruan</option>
+                                        <option value="Madiun">Madiun</option>
+                                        <option value="Kediri">Kediri</option>
+                                        <option value="Jember">Jember</option>
+                                        <option value="Banyuwangi">Banyuwangi</option>
+                                        <option value="Purwokerto">Purwokerto</option>
+                                        <option value="Cilacap">Cilacap</option>
+                                        <option value="Pekalongan">Pekalongan</option>
+                                        <option value="Tegal">Tegal</option>
+                                        <option value="Brebes">Brebes</option>
+                                        <option value="Karawang">Karawang</option>
+                                        <option value="Indramayu">Indramayu</option>
+                                        <option value="Cirebon">Cirebon</option>
+                                        <option value="Purwakarta">Purwakarta</option>
+                                        <option value="Sukabumi">Sukabumi</option>
+                                        <option value="Cianjur">Cianjur</option>
+                                        <option value="Bogor">Bogor</option>
+                                        <option value="Banjar">Banjar</option>
+                                        <option value="Ciamis">Ciamis</option>
+                                        <option value="Cirebon">Cirebon</option>
+                                        <option value="Tasikmalaya">Tasikmalaya</option>
+                                        <option value="Garut">Garut</option>
+                                        <option value="Pangandaran">Pangandaran</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="col-1">
@@ -52,15 +124,14 @@
                         <div class="row">
                             <div class="col-11">
                                 <div class="d-block">
-                                    <label for="form-availability-from">Check-In<small class="text-danger">*</small></label>
-                                    <input type="text" value="" class="form-control rounded text-left required" id="form-availability-from" name="form-availability-from" placeholder="MM/DD/YYYY">
+                                    <label for="form-availability-from">Tanggal Pemesanan<small class="text-danger">*</small></label>
+                                    <input type="date" value="" class="form-control rounded text-left required" id="form-availability-from" name="form-availability-from" placeholder="MM/DD/YYYY">
                                 </div>
-                                <div class="d-block ">
-                                    <div class="mb-3">
-                                        <label for="formGroupExampleInput" class="form-label">Durasi</label>
-                                        <input type="text " class="form-control " id="formGroupExampleInput" placeholder="Durasi">
-                                    </div>
+                               <div class="d-block">
+                                    <label for="form-availability-from">Tanggal Berangkat<small class="text-danger">*</small></label>
+                                    <input type="date" value="" class="form-control rounded text-left required" id="form-availability-from" name="form-availability-from" placeholder="DD/MM/YYYY">
                                 </div>
+                                
                             </div>
                             <div class="col-1">
                                 <div class="garis">
@@ -72,18 +143,8 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="d-block">
-                                    <label for="inputState" class="form-label">Lokasi Rental</label>
-                                    <select id="inputState" class="form-select">
-                                        <option selected></option>
-                                        <option>Jakarta</option>
-                                        <option>Bandung</option>
-                                        <option>Yogyakarta</option>
-                                        <option>Surabaya</option>
-                                        <option>Semarang</option>
-                                        <option>Malang</option>
-                                        <option>Bogor</option>
-                                        <option>Solo</option>
-                                    </select>
+                                    <label for="form-availability-from">Tanggal Kembali<small class="text-danger">*</small></label>
+                                    <input type="date" value="" class="form-control rounded text-left required" id="form-availability-from" name="form-availability-from" placeholder="MM/DD/YYYY">
                                 </div>
                                 <div class="d-block m-4">
                                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
